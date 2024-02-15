a7f66174c7e37e777f91fd24ebe942df6177f869
Jose Benavides P. <josbenav@iu.edu>
2024-02-15 18:09:52 -0500

    Updated file

--- 01_Encoding/encoding.py
+++ 01_Encoding/encoding.py
@@ -1,21 +1,29 @@
 import sys
 import re
 import string
+import unicodedata
 from collections import Counter
 
 text = sys.stdin.readlines()
 validchr = "aeiouáéíóúbcdfghijklmnñpqrstvwxyz"
-numbers = "01234567890"
+numbers = "1234567890"
 counts = Counter()
 
-characters = set(validchr + validchr.upper() + ' \n' + string.punctuation + numbers)
+ok_char = set(validchr + validchr.upper() + ' \n' + string.punctuation + numbers)
 
 for line in text:
-        if set(line) < characters:
+        if set(line) < ok_char:
                 sys.stdout.write(line)
         else:
-                counts.update(line)
-
+                counts.update([c for c in line if c not in ok_char])
+"""
 for char, total in counts.most_common():
-        if char not in characters:
+        if char not in ok_char:
+                cat = unicodedata.normalize('NFKC', char)
                 print(total, '\t', char, file=sys.stderr)
+"""
+for char, total in counts.most_common():
+        category = unicodedata.category(char)
+        if (char not in ok_char) and (category.startswith('Zs') or category.startswith('Cf')):
+                char = unicodedata.normalize('NFKC', char)
+        print('%d\t%s\t%x' % (total, char, ord(char)), file=sys.stderr)
